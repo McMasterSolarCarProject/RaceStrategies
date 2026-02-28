7706c0ad0bbb12fd88aa4cf5c48fd92e46f3b8da
adamaltenberg <adaltenberg@gmail.com>
2026-02-28 16:43:54 -0500

    updated to account for new optimization algorithms

--- src/optimizer/__init__.py
+++ src/optimizer/__init__.py
@@ -1,1 +1,7 @@
-from .optimize_velocity import optimize_route, simulate_with_v_eff, set_v_eff
+from .coarse_to_fine import set_v_eff, simulate_interval_with_v_eff
+from .common import OptResult, build_bounds, validate_with_full_sim, simulate_single_segment
+from .optimize_de import optimize_de
+from .optimize_slsqp import optimize_slsqp
+from .optimize_ga import optimize_ga
+from .optimize_dp import optimize_dp
+from .benchmark import benchmark_interval, optimize_route
